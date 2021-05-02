32b14491c4cf46645963c52acb1fb74f4cc05e80
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-02 23:35:12 +0530

    Update 011. Maxed_Out.py

--- 011. Maxed_Out.py
+++ 011. Maxed_Out.py
@@ -1,1 +1,22 @@
+l1 = [1, 7, 90, 33, 67, 56, 20, 39, 10, 30]
 
+def max_out(l1):
+    T = int(input())
+    l2 = []
+    for i in range(T):
+        a,b,c = map(int, input().split())
+        k = max(l1[a], l1[b], l1[c])
+        l2.append(k)
+    for i in l2:
+        print(i, end = "\n")
+
+max_out(l1)
+
+'''
+Expected Output:
+2
+1 2 3
+4 6 8
+90
+67
+'''

a48683f3df4648c6f8ca85ef28726d9c0a64387e
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-05 06:58:22 +0530

    Create 015. Multiplication_Table.py

---
+++ 015. Multiplication_Table.py
@@ -0,0 +1,52 @@
+#15. Multiplication Table
+
+T = int(input())
+l1 = []
+for _ in range(T):
+    l1.append(int(input()))
+for num in l1:
+    for j in range(1,11):    
+        print(str(num) + " x " + str(j) + " = " + str(num*j))
+        j += 1
+    print("===============")
+    
+'''
+Expected Output:
+3
+2
+4
+6
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+===============
+4 x 1 = 4
+4 x 2 = 8
+4 x 3 = 12
+4 x 4 = 16
+4 x 5 = 20
+4 x 6 = 24
+4 x 7 = 28
+4 x 8 = 32
+4 x 9 = 36
+4 x 10 = 40
+===============
+6 x 1 = 6
+6 x 2 = 12
+6 x 3 = 18
+6 x 4 = 24
+6 x 5 = 30
+6 x 6 = 36
+6 x 7 = 42
+6 x 8 = 48
+6 x 9 = 54
+6 x 10 = 60
+===============
+'''
